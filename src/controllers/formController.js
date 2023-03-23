6879d278e6624ac701c23a2830b43dd5c05cd6a5
mgsheld <123651790+mgsheld@users.noreply.github.com>
2023-03-23 15:19:09 -0500

    1 file modified, 13 added

---
+++ src/controllers/formController.js
@@ -0,0 +1,11 @@
+exports.form_post = function(req, res){
+  let birdData = {
+    birdSpecies: req.body.species,
+    birdNickname: req.body.nickname,
+    birdStatus: req.body.status
+  }
+  
+  console.log(birdData);
+  
+  res.render('form-submit-confirm', birdData)
+  };
